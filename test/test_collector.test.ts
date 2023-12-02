3ffad3bc24f78a1d8dd25faea4c7b63335501873
Hans Dushanthakumar <hans@hansm2promax.lan>
2023-12-03 00:38:37 +1100

    Initial commit

---
+++ test/test_collector.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TestCollector from '../lib/test_collector-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/test_collector-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TestCollector.TestCollectorStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
