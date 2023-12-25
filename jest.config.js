4b20357c27aec2768eb4082f4219b117fbf1f56a
Hans Dushanthakumar <hansrd_98@yahoo.com>
2023-12-26 01:02:49 +1100

    detailed test results

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,12 @@
 module.exports = {
   testEnvironment: 'node',
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
   roots: ['<rootDir>/test'],
   testMatch: ['**/*.test.ts'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
-  }
+  },
+  verbose: true
 };
