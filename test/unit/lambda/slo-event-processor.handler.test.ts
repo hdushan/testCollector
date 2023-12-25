d4679836e3582c2fe1639d899b53c2b56210e2f5
Hans Dushanthakumar <hansrd_98@yahoo.com>
2023-12-26 00:54:26 +1100

    fixed tests

--- test/unit/lambda/slo-event-processor.handler.test.ts
+++ test/unit/lambda/slo-event-processor.handler.test.ts
@@ -1,24 +1,28 @@
-import handler from 'src/lambda/slo-event-processor'
+import handler from '../../../src/lambda/slo-event-processor'
 import valid_slo_event from './fixtures/aws_slo_valid_event.json'
 import invalid_slo_event_empty_mandatory_fields from './fixtures/aws_slo_invalid_empty_fields.json'
 import invalid_slo_event_bad_env from './fixtures/aws_slo_invalid_environment.json'
 import invalid_slo_event_bad_state from './fixtures/aws_slo_invalid_event_state.json'
+import nr from 'newrelic'
 
 describe('SLOEventProcessot', () => {
-    // beforeEach(() => {
-    //     jest.mock('newrelic', () => ({ recordCustomEvent: jest.fn() }))
-    //     jest.mock('newrelic')
-    // });
+    beforeEach(() => {
+        process.env.NEW_RELIC_APP_NAME = 'TEST'
+        nr.recordCustomEvent = jest.fn()
+    })
 
-    // describe('valid slo event', () => {
-    //     it('should send the event to New Relic', async () => {
-    //         const recordsFromSQS = {
-    //             Records: [valid_slo_event]
-    //         }
-    //         await handler(recordsFromSQS)
-    //         expect(mockFunc).toHaveBeenCalledWith(null)
-    //     })
-    // })
+    describe('valid slo event', () => {
+        it('should send the event to New Relic', async () => {
+            const recordsFromSQS = {
+                Records: [valid_slo_event]
+            }
+            await handler(recordsFromSQS)
+            expect(nr.recordCustomEvent).toHaveBeenCalledWith(
+                'Analytics', 
+                JSON.parse(valid_slo_event.body).detail
+            )
+        })
+    })
 
     describe('invalid slo event', () => {
         describe('empty mandatory fields', () => {
@@ -27,8 +31,9 @@ describe('SLOEventProcessot', () => {
                     Records: [invalid_slo_event_empty_mandatory_fields]
                 }
                 expect(async () => {
-                    handler(recordsFromSQS)
+                    await handler(recordsFromSQS)
                 }).rejects.toThrow('SLO event failed validation:\nslo_name is empty')
+                expect(nr.recordCustomEvent).not.toHaveBeenCalled()
             })
         })
 
@@ -38,8 +43,9 @@ describe('SLOEventProcessot', () => {
                     Records: [invalid_slo_event_bad_state]
                 }
                 expect(async () => {
-                    handler(recordsFromSQS)
+                    await handler(recordsFromSQS)
                 }).rejects.toThrow('SLO event failed validation:\nevent_state has illegal value Junk. Allowed values are begin, success or failure')
+                expect(nr.recordCustomEvent).not.toHaveBeenCalled()
             })
         })
 
@@ -49,8 +55,9 @@ describe('SLOEventProcessot', () => {
                     Records: [invalid_slo_event_bad_env]
                 }
                 expect(async () => {
-                    handler(recordsFromSQS)
+                    await handler(recordsFromSQS)
                 }).rejects.toThrow('SLO event failed validation:\nenv has illegal value Junk. Allowed values are dev, qa, preprod or production')
+                expect(nr.recordCustomEvent).not.toHaveBeenCalled()
             })
         })
     })
